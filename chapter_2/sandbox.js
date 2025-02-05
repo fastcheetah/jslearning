45c33a2ac8f1d36937969a2515891febf4ae5c5e
Kelvin <edeh47245@gmail.com>
2025-02-05 19:35:32 +0100

    feat: Adding response status

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -513,8 +513,10 @@ console.log(new Date(timestamp));
 const request = new XMLHttpRequest();//this is an object that allows us to make requests to a server
 request.addEventListener('readystatechange',() => {
     console.log(request, request.readyState);
-    if(request.readyState === 4){
-        console.log(request.responseText);
+    if(request.readyState === 4 && request.status === 200){
+        console.log(request, request.responseText);
+    }else if (request.readyState === 4){
+        console.log("error fetching data");
     }
 });//this is an event listener that listens for the ready state change~~
 

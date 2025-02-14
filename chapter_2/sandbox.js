7987c3780f3d86b8f404f52d1530be24c04a78e7
Fastcheettahh <144325772+fastcheetah@users.noreply.github.com>
2025-02-14 23:35:19 +0100

    feat:intro to promises

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -539,4 +539,11 @@ if(err){
     console.log(data);
 } 
 });
-    
+
+//promise basics
+let promise = new Promise(function(resolve, reject) {
+  // the function is executed automatically when the promise is constructed
+
+  // after 1 second signal that the job is done with the result "done"
+  setTimeout(() => resolve("done"), 1000);
+});
